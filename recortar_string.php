0e9381f1801b7acc43cdea11ee93640596fdb1a4
ecr007 <evercuevas1000@gmail.com>
2014-03-10 11:43:28 -0700

    Create recortar_string.php

---
+++ recortar_string.php
@@ -0,0 +1,20 @@
+function recortar_texto($texto, $limite=100){   
+    $texto = trim($texto);
+    $texto = strip_tags($texto);
+    $tamano = strlen($texto);
+    $resultado = '';
+    if($tamano <= $limite){
+        return $texto;
+    }else{
+        $texto = substr($texto, 0, $limite);
+        $palabras = explode(' ', $texto);
+        $resultado = implode(' ', $palabras);
+        $resultado .= '...';
+    }   
+    return $resultado;
+}
+
+//Corrida
+
+$miTexto = "mucho texto .........";
+echo recortar_texto($miTexto,50); /*Texto, Cantidad de letras*/
